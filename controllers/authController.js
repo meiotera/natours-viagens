8d06a7471b3aa554f4df4f8f0c37a99c377dada4
Renan Ferreira <renanfc.re@gmail.com>
2024-06-29 11:48:42 -0300

    alteracao res.cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,7 +23,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwared-proto') === 'https',
+    secure: req.secure || req.headers['x-forwared-proto'] === 'https',
   });
 
   // removendo a senha da saida
